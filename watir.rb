5a224daa6dbbf6463313d84054c413b4321161d6
Christian LIM <christian.lim@outlook.fr>
2017-10-13 15:53:04 +0200

    first commit

---
+++ watir.rb
@@ -0,0 +1,19 @@
+require 'watir'
+browser = Watir::Browser.new
+browser.goto 'google.com'
+
+search_bar = browser.text_field(class: 'gsfi')
+
+search_bar.set("le tennis")
+
+search_bar.send_keys(:enter)
+
+submit_button = browser.button(type:"submit")
+submit_button.click
+
+browser.driver.manage.timeouts.implicit_wait = 10
+search_result_divs = browser.divs(class:"rc")
+search_result_divs.each { |div| p div.h3.text }
+
+p "Méfait accompli, fermeture du browser"
+browser.close
